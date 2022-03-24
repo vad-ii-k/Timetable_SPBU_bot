f176b9c1da9e584365763f1f5da556205e0611b8
Vadim <sazanov7615@gmail.com>
2022-03-24 17:47:57 +0300

    Creating the project architecture

---
+++ handlers/errors/error_handler.py
@@ -0,0 +1,23 @@
+import logging
+from aiogram.utils.exceptions import (TelegramAPIError,
+                                      MessageNotModified,
+                                      CantParseEntities)
+from loader import dp
+
+
+@dp.errors_handler()
+async def errors_handler(update, exception):
+
+    if isinstance(exception, MessageNotModified):
+        logging.exception('Message is not modified')
+        return True
+      
+    if isinstance(exception, CantParseEntities):
+        logging.exception(f'CantParseEntities: {exception} \nUpdate: {update}')
+        return True
+
+    if isinstance(exception, TelegramAPIError):
+        logging.exception(f'TelegramAPIError: {exception} \nUpdate: {update}')
+        return True
+
+    logging.exception(f'Update: {update} \n{exception}')
