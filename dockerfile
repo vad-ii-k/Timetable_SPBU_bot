633a5a1320a596f41197156d1c1f910c39a00db0
vad-ii-k <sazanov7615@gmail.com>
2022-10-11 18:36:45 +0300

    🔥 Add: chromium installation for pyppeteer

--- dockerfile
+++ dockerfile
@@ -6,3 +6,6 @@ WORKDIR /usr/src/app/"${BOT_NAME:-tg_bot}"
 COPY requirements.txt /usr/src/app/"${BOT_NAME:-tg_bot}"
 RUN pip install -r /usr/src/app/"${BOT_NAME:-tg_bot}"/requirements.txt
 COPY . /usr/src/app/"${BOT_NAME:-tg_bot}"
+
+#Installing chromium for pyppeteer
+RUN apk -U add chromium udev ttf-freefont
--- tgbot/services/image_converter.py
+++ tgbot/services/image_converter.py
@@ -1,6 +1,7 @@
 import io
+import os
 
-import imgkit
+import pyppeteer
 from PIL import Image
 from jinja2 import Environment, FileSystemLoader
 from aiogram.types import BufferedInputFile
@@ -14,11 +15,24 @@ async def image_to_buffered_input_file() -> BufferedInputFile:
     with open(results_filename, mode="w", encoding="utf-8") as results:
         results.write(results_template.render())
 
-    imgkit.from_file(results_filename, 'data/output.png')
+    browser = await pyppeteer.launch(
+        headless=True,
+        executablePath="/usr/bin/chromium-browser",
+        args=['--no-sandbox', '--disable-gpu']
+    )
+    page_for_psv = await browser.newPage()
+    await page_for_psv.goto(f'file:///{os.path.abspath("data/compiled_html_pages/schedule.html")}')
+    await page_for_psv.screenshot(
+        path='data/output.png',
+        type='jpeg',
+        fullPage=True,
+        quality=100
+    )
 
     image = Image.open(r"data/output.png")
     image_byte_arr = io.BytesIO()
     image.save(image_byte_arr, format=image.format)
     image_byte_arr = image_byte_arr.getvalue()
     buffered_photo = BufferedInputFile(file=image_byte_arr, filename="output.png")
+    await browser.close()
     return buffered_photo
