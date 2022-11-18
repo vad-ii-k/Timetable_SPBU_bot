8d2e47a498fba9d2d0fbf9d6c18fd34b194f1f70
vad-ii-k <sazanov7615@gmail.com>
2022-11-18 19:28:36 +0300

    Update: chat actions for schedule type

--- tgbot/handlers/schedule.py
+++ tgbot/handlers/schedule.py
@@ -2,6 +2,7 @@
 import asyncio
 
 from aiogram import Router, F, flags
+from aiogram.filters import and_f
 from aiogram.types import CallbackQuery
 
 from tgbot.misc.cb_data import ScheduleCallbackFactory
@@ -74,9 +75,9 @@ async def schedule_weeks_callback(callback: CallbackQuery, callback_data: Schedu
     asyncio.create_task(delete_message(callback.message, 0))
 
 
-@router.callback_query(ScheduleCallbackFactory.filter(F.button == "3-1"))
+@router.callback_query(and_f(ScheduleCallbackFactory.filter(F.button == "3-1"), F.message.document))
 @flags.chat_action('typing')
-async def schedule_photo_callback(callback: CallbackQuery, callback_data: ScheduleCallbackFactory):
+async def schedule_document_callback(callback: CallbackQuery, callback_data: ScheduleCallbackFactory):
     """
     Handling button clicks to change the schedule view using a scheduled keyboard
     :param callback:
@@ -84,20 +85,31 @@ async def schedule_photo_callback(callback: CallbackQuery, callback_data: Schedu
     """
     await change_message_to_loading(callback.message)
     tt_id, user_type = callback_data.tt_id, callback_data.user_type
-    day_counter, week_counter = callback_data.day_counter, callback_data.week_counter
-    if callback.message.content_type == "document":
-        if week_counter is not None:
-            text, _ = await get_text_week_schedule(tt_id, user_type, week_counter=week_counter)
-            await schedule_keyboard_helper(callback, callback_data, text)
-        else:
-            text = await get_text_day_schedule(tt_id, user_type, day_counter=day_counter)
-            await schedule_keyboard_helper(callback, callback_data, text)
+    if callback_data.week_counter is not None:
+        text, _ = await get_text_week_schedule(tt_id, user_type, week_counter=callback_data.week_counter)
+        await schedule_keyboard_helper(callback, callback_data, text)
     else:
-        if week_counter is not None:
-            text, photo = await get_image_week_schedule(tt_id, user_type, week_counter=week_counter)
-            await schedule_keyboard_helper(callback, callback_data, text, photo)
-        else:
-            text, photo = await get_image_day_schedule(tt_id, user_type, day_counter=day_counter)
-            await schedule_keyboard_helper(callback, callback_data, text, photo)
+        text = await get_text_day_schedule(tt_id, user_type, day_counter=callback_data.day_counter)
+        await schedule_keyboard_helper(callback, callback_data, text)
+    await callback.answer(cache_time=2)
+    asyncio.create_task(delete_message(callback.message, 0))
+
+
+@router.callback_query(and_f(ScheduleCallbackFactory.filter(F.button == "3-1"), ~F.message.document))
+@flags.chat_action('upload_document')
+async def schedule_text_callback(callback: CallbackQuery, callback_data: ScheduleCallbackFactory):
+    """
+    Handling button clicks to change the schedule view using a scheduled keyboard
+    :param callback:
+    :param callback_data:
+    """
+    await change_message_to_loading(callback.message)
+    tt_id, user_type = callback_data.tt_id, callback_data.user_type
+    if callback_data.week_counter is not None:
+        text, photo = await get_image_week_schedule(tt_id, user_type, week_counter=callback_data.week_counter)
+        await schedule_keyboard_helper(callback, callback_data, text, photo)
+    else:
+        text, photo = await get_image_day_schedule(tt_id, user_type, day_counter=callback_data.day_counter)
+        await schedule_keyboard_helper(callback, callback_data, text, photo)
     await callback.answer(cache_time=2)
     asyncio.create_task(delete_message(callback.message, 0))
